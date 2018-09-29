ded827ab4f81cb199083ea46102ffa7e25cd1226
toropov <toropov023@gmail.com>
2018-09-29 08:28:47 -0400

    Info and status texts; a bit of styling

--- src/main/java/ca/toropov/microcad/fogPrettifier/Prettifier.java
+++ src/main/java/ca/toropov/microcad/fogPrettifier/Prettifier.java
@@ -1,20 +1,21 @@
 package ca.toropov.microcad.fogPrettifier;
 
 import javafx.application.Platform;
+import javafx.scene.paint.Color;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.Consumer;
+import java.util.function.BiConsumer;
 
 /**
  * Author: toropov
  * Date: 9/28/2018
  */
 class Prettifier {
-    static void prettify(List<File> files, Consumer<String> callback) {
+    static void prettify(List<File> files, BiConsumer<String, Color> callback) {
         ThreadUtil.onNewThread(() -> {
             AtomicInteger failed = new AtomicInteger();
             for (File file : files) {
@@ -23,7 +24,7 @@ class Prettifier {
                 }
             }
 
-            Platform.runLater(() -> callback.accept(failed.get() == 0 ? "Success!" : ("Failed to load " + failed.get() + " files out of " + files.size())));
+            Platform.runLater(() -> callback.accept(failed.get() == 0 ? "Success!" : ("Failed to load " + failed.get() + " files out of " + files.size()), failed.get() == 0 ? Color.LIGHTGREEN : Color.RED));
         });
     }
 
